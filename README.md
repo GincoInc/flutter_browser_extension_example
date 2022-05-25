a3d711f2a96aeb764f628bbb525510c189c36ddb
muraikenta <muraikenta0507@gmail.com>
2022-05-25 17:48:52 +0900

    拡張機能動くまで

---
+++ README.md
@@ -0,0 +1,7 @@
+# flutter_browser_extension_example
+
+## Run as Chrome Extension
+
+1. `fvm flutter build web --web-renderer html --csp`
+2. Access chrome://extensions/ in Chrome's URL bar.
+3. 「パッケージ化されていない拡張機能を読み込む」で build/web フォルダを選択
---
+++ web/bootstrap.js
@@ -0,0 +1,17 @@
+var serviceWorkerVersion = null;
+
+window.addEventListener("load", function (ev) {
+  // Download main.dart.js
+  _flutter.loader
+    .loadEntrypoint({
+      serviceWorker: {
+        serviceWorkerVersion: serviceWorkerVersion,
+      },
+    })
+    .then(function (engineInitializer) {
+      return engineInitializer.initializeEngine();
+    })
+    .then(function (appRunner) {
+      return appRunner.runApp();
+    });
+});

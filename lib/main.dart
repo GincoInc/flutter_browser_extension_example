293ea27c8774481822a97b9148651b6570fb5d0c
muraikenta <muraikenta0507@gmail.com>
2022-05-29 22:53:01 +0900

    Add web3 example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+// ignore_for_file: avoid_print, prefer_interpolation_to_compose_strings, prefer_const_constructors
+
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:bip39/bip39.dart' as bip39;
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +36,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    web3dartExample();
+  }
+
+  void web3dartExample() async {
+    // generate a new key randomly
+    var rng = Random.secure();
+
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    String randomMnemonic = bip39.generateMnemonic();
+    print('randomMnemonic:' + randomMnemonic);
+    String seed = bip39.mnemonicToSeedHex(randomMnemonic);
+    print('seed: ' + seed);
+    String entropy = bip39.mnemonicToEntropy(randomMnemonic);
+    print('entropy: ' + entropy);
+    print("----------------------------------");
+
+    seed =
+        '4d294ec54422ab8400ae0acb77a185dc501116bf9feb2453cc06e2cc60344c75188f2c8c690089333b1a7134ef8a53fac75156b00641e234a780131f1c69fe99';
+    final credentials = EthPrivateKey.fromHex(seed);
+    Wallet wallet = Wallet.createNew(credentials, "password", rng);
+    print(wallet.toJson());
+    print('address: ' + wallet.privateKey.address.hex);
+  }
+
   void subscribe() {
     final coinPriceStream = FirebaseFirestore.instance
         .collection('coinPrices')
@@ -55,8 +88,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: [
             TextButton(
+              onPressed: subscribe,
               child: Text("subscribe"),
-              onPressed:subscribe,
             )
           ],
         ),
